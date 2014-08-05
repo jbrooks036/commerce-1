ec8912c703a3271b04689a7de7e6ceffc25486c0
JoAnn Brooks <joannbrooks036@gmail.com>
2014-08-05 11:16:17 -0500

    refactored view files

--- app/lib/pipeline.js
+++ app/lib/pipeline.js
@@ -2,6 +2,7 @@
 
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var methodOverride = require('express-method-override');
 var home = require('../controllers/home');
 var items = require('../controllers/items');
 
@@ -9,6 +10,7 @@ module.exports = function(app, express){
   app.use(morgan('dev'));
   app.use(express.static(__dirname + '/../static'));
   app.use(bodyParser.urlencoded({extended:true}));
+  app.use(methodOverride());
 
   app.get('/', home.index);
   app.get('/about', home.about);
